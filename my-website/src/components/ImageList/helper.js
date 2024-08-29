9fc79e31bbccbf55891ff6a3c4f1ea18cbb30bd9
MelsMustCode <melsmustscreamofficial@gmail.com>
2024-08-28 20:33:35 -0400

    First Commit

---
+++ my-website/src/components/ImageList/helper.js
@@ -0,0 +1,91 @@
+import { fourPanel } from '../Images/CrManzana/SPOILER_xolomon4k.png';
+import { reactive } from '../Images/funayfunay/melscomm_IDLE.png';
+import { scuffGrem } from '../Images/KingBumby/scuffgremlin_melsmustscream.png';
+import { sakuraMels } from '../Images/KingofDawn0/background.PNG';
+import { falling } from '../Images/Makishupy/whatsAppImage.jpeg';
+import { kaguraMels } from '../Images/suamingVT/mel_kagura_bachi.png';
+import { tangoWithSol } from '../Images/WaNN/Sol_and_Mel.png';
+import { chibi } from '../Images/tuliblu/Xolo_2.png';
+import { dawnling } from '../Images/MelsMustScream/Dawnling.png'
+
+export const itemData = [
+    {
+      img: scuffGrem,
+      title: 'Scuff Gremlin',
+      author: '@KINGBumby',
+      rows: 2,
+      cols: 2,
+      featured: true,
+      // https://x.com/KINGBumby
+    },
+    {
+      img: kaguraMels,
+      title: 'kaguraMels',
+      author: '@suamingVT',
+      // https://x.com/suamingVT
+    },
+    {
+      img: sakuraMels,
+      title: 'Sakura Mels',
+      author: '@KingofDawn0',
+      // https://x.com/KingofDawn0
+    },
+    {
+      img: tangoWithSol,
+      title: 'Ice cream so sweet, it makes my heart dance',
+      author: '@WaNN',
+      cols: 2,
+    },
+    // {
+    //   img: falling,
+    //   title: 'Hats',
+    //   author: '@hjrc33',
+    //   cols: 2,
+    // },
+    {
+      img: falling,
+      title: 'Falling for Ice Cream',
+      author: '@Makishupy',
+      rows: 2,
+      cols: 2,
+      featured: true,
+      // https://x.com/Makishupy
+    },
+    {
+      img: reactive,
+      title: 'Official PNGtuber',
+      author: '@funayfunay',
+      // https://x.com/funayfunay
+    },
+    {
+      img: fourPanel,
+      title: 'Your Honor This is a Denny\'s',
+      author: '@CrManzana',
+      // https://x.com/CrManzana
+    },
+    {
+      img: chibi,
+      title: 'Chibi~',
+      author: '@tuliblu',
+      // https://x.com/tuliblu/status/1805303849193742721
+      rows: 2,
+      cols: 2,
+    },
+    {
+      img: dawnling,
+      title: 'Dawnling',
+      author: '@MelsMustScream',
+      // https://x.com/MelsMustScream
+    },
+    // {
+    //   img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
+    //   title: 'Sea star',
+    //   author: '@peterlaster',
+    // },
+    // {
+    //   img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
+    //   title: 'Bike',
+    //   author: '@southside_customs',
+    //   cols: 2,
+    // },
+  ];
---
+++ my-website/src/pages/index.tsx
@@ -0,0 +1,47 @@
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import Heading from '@theme/Heading';
+import TitlebarImageList from '../components/ImageList';
+
+import styles from './index.module.css';
+
+// so this top thing is the header at the top
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <Heading as="h1" className="hero__title">
+          {"website title"} 
+        </Heading>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/intro">
+            Docusaurus Tutorial - 5min ⏱️
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+// this here is the main website we see on landing 
+export default function Home(): JSX.Element {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Layout
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />">
+      {/* <HomepageHeader /> */}
+      <main>
+        {/* <HomepageFeatures /> */}
+        <TitlebarImageList />
+      </main>
+    </Layout>
+  );
+}
