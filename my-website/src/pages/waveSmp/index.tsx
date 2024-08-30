802fe403b0946ef4bb1a07e9d3cdad8f485a9cd2
MelsMustCode <melsmustscreamofficial@gmail.com>
2024-08-30 18:36:38 -0400

    cleaning up titles

--- my-website/src/pages/waveSmp/index.tsx
+++ my-website/src/pages/waveSmp/index.tsx
@@ -1,15 +1,16 @@
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import TitlebarImageList from '@site/src/components/ImageList';
 
 export default function ArtShowcase(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Art Showcase | ${siteConfig.title}`}
+      title={`WaveSMP | ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <main>
-        <TitlebarImageList />
+        {/* <WaveSMPLandingPage /> */}
       </main>
     </Layout>
   );
