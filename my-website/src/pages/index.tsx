949343c1c1747c570aa789097672a9f9bbfe8ad5
MelsMustCode <melsmustscreamofficial@gmail.com>
2024-08-30 18:36:27 -0400

    Landing Page

--- my-website/src/pages/index.tsx
+++ my-website/src/pages/index.tsx
@@ -1,20 +1,15 @@
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import TitlebarImageList from '../components/ImageList';
+import { LandingPage } from '../components/LandingPage';
 
-import styles from './index.module.css';
-
-// this here is the main website we see on landing 
 export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`Home | ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <main>
-        {/* <TitlebarImageList /> */}
+        <LandingPage />
       </main>
     </Layout>
   );
