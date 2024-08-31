eec9b480939fb9558a956ffabc6323474155b10d
MelsMustCode <melsmustscreamofficial@gmail.com>
2024-08-31 18:50:34 -0400

    Moving up files

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import { LandingPage } from './LandingPage';
+
+export default function Home(): JSX.Element {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Layout
+      title={`Home | ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />">
+      <main>
+        <LandingPage />
+      </main>
+    </Layout>
+  );
+}
