d54358178794e812c25f28dae235abeed2aeae5c
MelsMustCode <melsmustscreamofficial@gmail.com>
2024-09-01 13:43:45 -0400

    text over image

--- src/pages/waveSmp/index.tsx
+++ src/pages/waveSmp/index.tsx
@@ -4,11 +4,13 @@ import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import { Box } from "@mui/material";
 import styles from "../LandingPage/index.module.css";
+import MainBanner from "@site/src/components/MainBanner";
 
 export default function WaveSMPLandingPage(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout>
+      <MainBanner />
       <header className={clsx("hero", styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{'WaveSMP'}</h1>
